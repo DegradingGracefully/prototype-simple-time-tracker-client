d3a15d3a5e96dadfa99c41889545ca6a3801207e
Plastique Brian <adressediffusable@gmail.com>
2021-01-10 10:03:46 +0100

    v0.21.0 - minor
    * renamed newDurationHelper.formattedDisplay to formattedDuration
    * added comment to newDurationHelper class

--- src/TodoList.svelte
+++ src/TodoList.svelte
@@ -37,25 +37,31 @@
 
   let interval;
 
+/*  The newDuratonHelper class is a container for the "original" number of seconds of the task, as well as
+    its "floored" hours, minutes, and seconds + the formatted string (ie the "{hh}hmm{m}" string format we display in the UI)
+    Use: we don't provide any "updateSeconds" method. Indeed, you need to recreate a new newDurationHelper each time,
+    which is not more costly than an update would be, since if updating the seconds we would have to rerun
+    all the calculations.
+*/
 function newDurationHelper(seconds) {
   const secondsOriginal = seconds;
   const hoursFloored = Math.floor(seconds / 3600);
   const minutesFloored = Math.floor((seconds % 3600) / 60);
   const secondsFloored = hoursFloored * 3600 + minutesFloored * 60;
-  let formattedDisplay;
+  let formattedDuration;
 
   if (secondsOriginal === 0) {
-    formattedDisplay = TEXT_TASK_TO_BEGIN;
+    formattedDuration = TEXT_TASK_TO_BEGIN;
   } else if (hoursFloored === 0) {
     // less than an hour => only minutes
-    formattedDisplay = `${minutesFloored}m`;
+    formattedDuration = `${minutesFloored}m`;
   } else {
     // more than an hour
     let minutesPart = minutesFloored;
     if (minutesPart < 10) {
       minutesPart = `0${minutesPart}`;
     }
-    formattedDisplay = `${hoursFloored}h${minutesPart}`;
+    formattedDuration = `${hoursFloored}h${minutesPart}`;
   }
 
   return {
@@ -69,9 +75,9 @@ function newDurationHelper(seconds) {
       return secondsFloored;
     },
     getFormattedDuration() {
-      /* console.log('secondsOriginal / hoursFloored / minutesFloored / secondsFloored / formattedDisplay');
-      console.log(`${secondsOriginal} / ${hoursFloored} / ${minutesFloored} / ${secondsFloored} / ${formattedDisplay}`); */
-      return formattedDisplay;
+      /* console.log('secondsOriginal / hoursFloored / minutesFloored / secondsFloored / formattedDuration');
+      console.log(`${secondsOriginal} / ${hoursFloored} / ${minutesFloored} / ${secondsFloored} / ${formattedDuration}`); */
+      return formattedDuration;
     },
   };
 }
